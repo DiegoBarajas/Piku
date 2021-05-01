fd456062f4aaeca2d4f56e79aa4c15a8b7c417ef
DiegoBarajas <81001205+DiegoBarajas@users.noreply.github.com>
2021-04-30 23:29:35 -0500

    Agregue el inicio de sesion a la app

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-var express = require("express");
+const express = require("express");
 const Cloudant = require ("@cloudant/cloudant");
-var app = express();
+const app = express();
+const PORT = 8080;
 
 app.use("/public",express.static("public"));
 
@@ -8,61 +9,120 @@ app.use(express.json()); //Peticiones con formato application/json
 app.use(express.urlencoded({extended: true}));
 
 app.set("view engine","pug");
-const PORT = 3000;
 
+
+//------------------------- INDEX -------------------------\\
 app.get("/",(req,res)=>{
     res.render("index");
 });
 
+
+//------------------------- INICIAR SESION -------------------------\\
 app.get("/login_al",(req,res)=>{
-    res.render("login");    
+    res.render("login_al");
 });
 
-app.post("/users",(req,res)=>{
+app.get("/login_mt",(req,res)=>{
+    res.render("login_mt");
+});
 
-    //---------------------------------------- Crear DB ----------------------------------------//
 
-    cloudant();
-    async function cloudant(){
-        try {
-            console.log("Creando conexion con base de datos....");
-            const cloudant = Cloudant({
-                url:"https://9f54e758-3ad6-4391-8439-003d07506891-bluemix.cloudantnosqldb.appdomain.cloud",
-                plugins:{
-                    iamauth:{
-                        iamApiKey: "BXXfOZYJWpnnPykjZcJSJ8pOtuuADMw9M_mrxZ0IRum0"
+//------------------------- CREAR CUENTA -------------------------\\
+app.get("/signin_al",(req,res)=>{
+    res.render("signin_al");
+});
+
+app.get("/signin_mt",(req,res)=>{
+    res.render("signin_mt");
+});
+
+
+//------------------------- NUEVO USUARIO -------------------------\\
+app.post("/new_user",(req,res)=>{
+    //Se compara que las contraseñas existen si no es alumno
+    if (req.body.password == undefined){
+
+        //Creando usuario en la base de datos
+        cloudant();
+        async function cloudant(){
+            try{
+                console.log("Creando Conexion con la Base de Datos.....");
+                const cloudant = Cloudant({
+                    url: "https://9f54e758-3ad6-4391-8439-003d07506891-bluemix.cloudantnosqldb.appdomain.cloud",
+                    plugins: {
+                        iamauth:{
+                            iamApiKey: "BXXfOZYJWpnnPykjZcJSJ8pOtuuADMw9M_mrxZ0IRum0"
+                        }
                     }
-                }
-            });
-        console.log("Conexion creada");
+                });
+                console.log("Conexion con Base de Datos creada");
 
-        var JSON_al = {
-            "user_type": "alumn",
-            "name": req.body.name,
-            "lastname": req.body.lastname,
-            "email": req.body.email,
-            "birthday": req.body.birthday,
-            "pikoins": 0,
-            "class_code": null
+                const piku_users = cloudant.db.use("piku_alumn");
+                new_user = await piku_users.insert({"_id":req.body.email,
+                    "usertype": "alumno",
+                    "name":req.body.name,
+                    "lastname":req.body.lastname,
+                    "email" :req.body.email,
+                    "birthday": req.body.birthday,
+                    "classcode":undefined, 
+                    "pikoins":0
+                });
+                console.log("Documento Creado en piku_alumn");
+
+                res.render("new_user_al");
+            }catch(err){
+                console.error(err);
+                res.render("id_registered");
+            }
         }
-            const db = cloudant.db.use("piku_alumn");
-            let res="";
-            doc_al = JSON_al;
-            res = db.insert(doc_al);
-            console.log("Agregando DB");
-        }catch(err){
-            console.log(err);
+    //En caso de que la contraseña exita se comparan si son igales la contraeña y la confirmacion
+    }else if(req.body.password == req.body.password_c){
+        //Creando usuario en la base de datos
+        cloudant();
+        async function cloudant(){
+            try{
+                console.log("Creando Conexion con la Base de Datos.....");
+                const cloudant = Cloudant({
+                    url: "https://9f54e758-3ad6-4391-8439-003d07506891-bluemix.cloudantnosqldb.appdomain.cloud",
+                    plugins: {
+                        iamauth:{
+                            iamApiKey: "BXXfOZYJWpnnPykjZcJSJ8pOtuuADMw9M_mrxZ0IRum0"
+                        }
+                    }
+                });
+                console.log("Conexion con Base de Datos creada");
+
+                const piku_users = cloudant.db.use("piku_mtrs");
+                new_user = await piku_users.insert({"_id":req.body.email,
+                    "usertype": "maestro",
+                    "name":req.body.name,
+                    "lastname":req.body.lastname,
+                    "email" :req.body.email,
+                    "password":req.body.password,
+                    "birthday": req.body.birthday,
+                    "classcode":undefined, 
+                    "pikoins":0
+                });
+                console.log("Documento Creado en piku_mtrs");
+
+                res.render("new_user_mt");
+            }catch(err){
+                console.error(err);
+                res.render("id_registered");
+            }
         }
+    //Si no coinciden marca error
+    }else if(req.body.password !== req.body.password_c){
+        res.render("wrong_password")
     }
+});
 
-    //-----------------------------------------------------------------------------------------//
 
-
-    console.log("Nombre: " + req.body.name);
-    console.log("Apellido: " + req.body.lastname);
-    console.log("Correo o Numero: " + req.body.email);
-    console.log("Fecha de Nacimiento: " + req.body.birthday);
-    res.render("users");
+//------------------------- USUARIO -------------------------\\
+app.post("/user",(req,res)=>{
+    res.send("User");
+    console.log("Email: "+req.body.email);
+    console.log("Password: "+req.body.password);
 });
 
 app.listen(PORT,()=>{
