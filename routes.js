a161acd766f45d4e712a2e0cf64e5b95a2db99c8
DiegoBarajas <81001205+DiegoBarajas@users.noreply.github.com>
2021-05-03 21:50:35 -0500

    Arregle un Bug pequeño

--- routes.js
+++ routes.js
@@ -5,7 +5,7 @@ router.get("/",(req,res)=>{
     res.render("app/index",{name: req.session.user_name, lastname: req.session.user_lastname, email: req.session.user_id, birthday: req.session.user_birthday, pikoins: req.session.user_pikoins, classcode: req.session.user_classcode});
 });
 router.get("/myuser",(req,res)=>{
-    res.render("app/myuser",{name: req.session.user_name, lastname: req.session.user_lastname, email: req.session.user_id, birthday: req.session.user_birthday, pikoins: req.session.user_pikoins, classcode: req.session.user_classcode});
+    res.render("app/myuser",{name: req.session.user_name, lastname: req.session.user_lastname, email: req.session.user_id, birthday: req.session.user_birthday, pikoins: req.session.user_pikoins, classcode: req.session.user_classcode, usertype: req.session.user_type});
 });
 
 module.exports = router;
