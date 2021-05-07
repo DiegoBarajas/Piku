9913422aaaad23cfbc7a75b96deffe874a2eaabf
DiegoBarajas <81001205+DiegoBarajas@users.noreply.github.com>
2021-05-07 09:11:43 -0500

    Arregle algunos errores de syntaxis

--- routes_class.js
+++ routes_class.js
@@ -2,7 +2,7 @@ const express = require("express");
 var router = express.Router();
 
 router.get("/",(req,res)=>{
-    res.send("Clase");
+    res.render("clase/index");
 });
 
 module.exports = router;
